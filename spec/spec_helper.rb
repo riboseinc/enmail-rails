8824fbfcd5285fd49c3097e65b1b648f3bab11af
Sebastian Skałacki <skalee@gmail.com>
2018-08-11 06:55:17 +0200

    Make all development gems available in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ end
 
 require "bundler/setup"
 require "enmail/rails"
+Bundler.require(:default, :development)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
